693d67832fae9cf99c6a893dccae3f6f70fd5475
mh-cbon <mh-cbon@users.noreply.github.com>
2017-01-16 16:48:19 +0100

    TransformTree(minor): add a func to transform a parse.Tree only

--- simplifier/transform.go
+++ simplifier/transform.go
@@ -4,32 +4,35 @@ import (
 	"fmt"
 	html "html/template"
 	text "text/template"
+	"text/template/parse"
 )
 
-// Transform fully simplify a template Tree.
+// Transform fully simplify a template.
 // it accepts *text.Template or *html.Template,
 // it panics if the value type is unexpected.
 func Transform(some interface{}, data interface{}, funcs map[string]interface{}) {
 	if t, ok := some.(*text.Template); ok {
 		for _, tpl := range t.Templates() {
 			if tpl.Tree != nil {
-				Unshadow(tpl.Tree)
-				Simplify(tpl.Tree)
-				typeCheck := TypeCheck(tpl.Tree, data, funcs)
-				Unhole(tpl.Tree, typeCheck, funcs)
+				TransformTree(tpl.Tree, data, funcs)
 			}
 		}
 
 	} else if h, ok := some.(*html.Template); ok {
 		for _, tpl := range h.Templates() {
 			if tpl.Tree != nil {
-				Unshadow(tpl.Tree)
-				Simplify(tpl.Tree)
-				typeCheck := TypeCheck(tpl.Tree, data, funcs)
-				Unhole(tpl.Tree, typeCheck, funcs)
+				TransformTree(tpl.Tree, data, funcs)
 			}
 		}
 	} else {
 		panic(fmt.Errorf("Wrong type %T", some))
 	}
+}
+
+// TransformTree fully simplify a template Tree.
+func TransformTree(tree *parse.Tree, data interface{}, funcs map[string]interface{}) {
+	Unshadow(tree)
+	Simplify(tree)
+	typeCheck := TypeCheck(tree, data, funcs)
+	Unhole(tree, typeCheck, funcs)
 }
