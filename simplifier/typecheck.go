23f61a72292ef40715bbf37e34c5f043515c693a
mh-cbon <mh-cbon@users.noreply.github.com>
2017-01-16 08:46:44 +0100

    typechecker(minor): add a simple type checker

---
+++ simplifier/typecheck.go
@@ -0,0 +1,344 @@
+package simplifier
+
+import (
+	"fmt"
+	"reflect"
+	"text/template/parse"
+)
+
+// TypeCheck browses the tree to identify variable types.
+func TypeCheck(tree *parse.Tree, data interface{}, funcs map[string]interface{}) *State {
+	s := &State{
+		currentScope: -1,
+		vars:         []map[string]reflect.Type{},
+	}
+	t := &treeTypecheck{
+		funcs: funcs,
+		tree:  tree,
+	}
+	s.Add()
+	s.Enter()
+	s.AddVar(".", reflect.TypeOf(data))
+	t.process(tree, s)
+	s.Leave()
+	return s
+}
+
+// treeTypecheck ...
+type treeTypecheck struct {
+	tree  *parse.Tree
+	funcs map[string]interface{}
+}
+
+// State ...
+type State struct {
+	currentScope int
+	vars         []map[string]reflect.Type
+}
+
+// Add a new scope level.
+func (s *State) Add() {
+	s.vars = append(s.vars, map[string]reflect.Type{})
+}
+
+// Enter into a scope level.
+func (s *State) Enter() {
+	s.currentScope++
+}
+
+// Len returns the number of scopes.
+func (s *State) Len() int {
+	return len(s.vars)
+}
+
+// Level returns the current scope level.
+func (s *State) Level() int {
+	return s.currentScope
+}
+
+// Leave a scope level.
+func (s *State) Leave() {
+	s.currentScope--
+}
+
+// Current scope vars.
+func (s *State) Current() map[string]reflect.Type {
+	return s.vars[s.currentScope]
+}
+
+// Root scope vars.
+func (s *State) Root() map[string]reflect.Type {
+	return s.vars[0]
+}
+
+// Dot is the current scope dot.
+func (s *State) Dot() reflect.Type {
+	return s.Current()["."]
+}
+
+// RootDot is the root scope dot.
+func (s *State) RootDot() reflect.Type {
+	return s.Root()["."]
+}
+
+// AddVar in the current scope level.
+func (s *State) AddVar(name string, r reflect.Type) {
+	s.Current()[name] = r
+}
+
+// HasVar tells if current level contains given variable name.
+func (s *State) HasVar(name string) bool {
+	_, ok := s.Current()[name]
+	return ok
+}
+
+// GetVar get a variable in current scope.
+func (s *State) GetVar(name string) reflect.Type {
+	r, _ := s.Current()[name]
+	return r
+}
+
+// FindVar starting from current scope level to the root.
+func (s *State) FindVar(name string) reflect.Type {
+	for i := s.currentScope; i >= 0; i-- {
+		if v, ok := s.vars[i][name]; ok {
+			return v
+		}
+	}
+	if name == "$" {
+		return s.RootDot()
+	}
+	return nil
+}
+
+// process the tree until no more simplification can be done.
+func (t *treeTypecheck) process(tree *parse.Tree, state *State) {
+	t.browseNodes(tree.Root, state)
+}
+
+// browseNodes recursively.
+func (t *treeTypecheck) browseNodes(l interface{}, state *State) {
+	switch node := l.(type) {
+
+	case *parse.ListNode:
+		if node != nil {
+			for _, child := range node.Nodes {
+				t.browseNodes(child, state)
+			}
+		}
+
+	case *parse.ActionNode:
+		t.typeCheckActionNode(node, state)
+		t.browseNodes(node.Pipe, state)
+
+	case *parse.RangeNode:
+		t.enterRangeNode(node, state)
+		t.browseNodes(node.Pipe, state)
+		t.browseNodes(node.List, state)
+		t.browseNodes(node.ElseList, state)
+		state.Leave()
+
+	case *parse.IfNode:
+		t.browseNodes(node.Pipe, state)
+		t.browseNodes(node.List, state)
+		t.browseNodes(node.ElseList, state)
+
+	case *parse.WithNode:
+		t.enterWithNode(node, state)
+		t.browseNodes(node.Pipe, state)
+		t.browseNodes(node.List, state)
+		t.browseNodes(node.ElseList, state)
+		state.Leave()
+
+	case *parse.PipeNode:
+		for _, c := range node.Decl {
+			t.browseNodes(c, state)
+		}
+		for _, c := range node.Cmds {
+			t.browseNodes(c, state)
+		}
+
+	case *parse.CommandNode:
+		for _, c := range node.Args {
+			t.browseNodes(c, state)
+		}
+
+	case *parse.VariableNode:
+		//pass
+	case *parse.IdentifierNode:
+		//pass
+	case *parse.StringNode:
+		//pass
+	case *parse.NumberNode:
+		//pass
+	case *parse.BoolNode:
+		//pass
+	case *parse.DotNode:
+		//pass
+	case *parse.FieldNode:
+		//pass
+	case *parse.TextNode:
+		//pass
+
+	default:
+		fmt.Printf("%#v\n", node)
+		fmt.Printf("!!! Unhandled %T\n", node)
+		panic("unhandled")
+	}
+}
+
+func (t *treeTypecheck) typeCheckActionNode(node *parse.ActionNode, state *State) bool {
+	/*
+		looks for
+		{{$some := .Field.Node}}
+		{{$some := $y}}
+		{{$some := $y.Field}}
+		{{$some := .}}
+		{{$some := up $w}}
+		{{$some := "what"}}
+		{{$some := 4}}
+		{{$some := true}}
+	*/
+	if len(node.Pipe.Decl) > 0 && len(node.Pipe.Decl[0].Ident) == 1 {
+		varName := node.Pipe.Decl[0].Ident[0]
+		if len(node.Pipe.Cmds) == 1 && len(node.Pipe.Cmds[0].Args) > 0 {
+			if field, ok := node.Pipe.Cmds[0].Args[0].(*parse.FieldNode); ok {
+				r := t.getPathType(field.Ident, state.Dot())
+				state.AddVar(varName, r)
+
+			} else if variable, ok := node.Pipe.Cmds[0].Args[0].(*parse.VariableNode); ok {
+				rightVarType := state.FindVar(variable.Ident[0])
+				if len(variable.Ident) > 1 {
+					rightVarType = t.getPathType(variable.Ident[1:], rightVarType)
+				}
+				state.AddVar(varName, rightVarType)
+
+			} else if _, ok := node.Pipe.Cmds[0].Args[0].(*parse.DotNode); ok {
+				rightVarType := state.Dot()
+				state.AddVar(varName, rightVarType)
+
+			} else if ident, ok := node.Pipe.Cmds[0].Args[0].(*parse.IdentifierNode); ok {
+				funcRetType := t.getFuncValueType(ident.Ident)
+				state.AddVar(varName, funcRetType)
+
+			} else if _, ok := node.Pipe.Cmds[0].Args[0].(*parse.StringNode); ok {
+				state.AddVar(varName, reflect.TypeOf(""))
+
+			} else if _, ok := node.Pipe.Cmds[0].Args[0].(*parse.NumberNode); ok {
+				state.AddVar(varName, reflect.TypeOf(0))
+
+			} else if _, ok := node.Pipe.Cmds[0].Args[0].(*parse.BoolNode); ok {
+				state.AddVar(varName, reflect.TypeOf(true))
+
+			}
+		} else {
+			panic("unhandled")
+		}
+	}
+	return false
+}
+
+func (t *treeTypecheck) enterRangeNode(node *parse.RangeNode, state *State) bool {
+	var newDotType reflect.Type
+	if len(node.Pipe.Cmds) == 1 {
+		if variable, ok := node.Pipe.Cmds[0].Args[0].(*parse.VariableNode); ok {
+			//-
+			rightVarType := state.FindVar(variable.Ident[0])
+			if len(variable.Ident) > 1 {
+				rightVarType = t.getPathType(variable.Ident[1:], rightVarType)
+			}
+			newDotType = rightVarType
+
+		} else if _, ok := node.Pipe.Cmds[0].Args[0].(*parse.DotNode); ok {
+			newDotType = state.Dot()
+
+		} else {
+			panic("unhandled")
+		}
+	} else {
+		panic("unhandled")
+	}
+	if newDotType == nil {
+		panic("new dot type not found")
+	}
+	state.Add()
+	state.Enter()
+	state.AddVar(".", newDotType.Elem())
+	if len(node.Pipe.Decl) > 0 {
+		// add the new var to the new scope
+		if len(node.Pipe.Decl) == 1 {
+			state.AddVar(node.Pipe.Decl[0].Ident[0], state.Dot())
+
+		} else {
+			state.AddVar(node.Pipe.Decl[0].Ident[0], reflect.TypeOf(1))
+			state.AddVar(node.Pipe.Decl[1].Ident[0], state.Dot())
+		}
+	}
+	return false
+}
+
+func (t *treeTypecheck) enterWithNode(node *parse.WithNode, state *State) bool {
+	var newDotType reflect.Type
+	if len(node.Pipe.Cmds) == 1 {
+		if variable, ok := node.Pipe.Cmds[0].Args[0].(*parse.VariableNode); ok {
+			//-
+			rightVarType := state.FindVar(variable.Ident[0])
+			if len(variable.Ident) > 1 {
+				rightVarType = t.getPathType(variable.Ident[1:], rightVarType)
+			}
+			newDotType = rightVarType
+
+		} else if _, ok := node.Pipe.Cmds[0].Args[0].(*parse.DotNode); ok {
+			newDotType = state.Dot()
+
+		} else {
+			panic("unhandled")
+		}
+	} else {
+		panic("unhandled")
+	}
+	if newDotType == nil {
+		panic("new dot type not found")
+	}
+	state.Add()
+	state.Enter()
+	state.AddVar(".", newDotType)
+	if len(node.Pipe.Decl) > 0 {
+		// add the new var to the new scope
+		if len(node.Pipe.Decl) == 1 {
+			state.AddVar(node.Pipe.Decl[0].Ident[0], state.Dot())
+		} else {
+			panic("unhandled")
+		}
+	}
+	return false
+}
+
+func (t *treeTypecheck) getPathType(path []string, val reflect.Type) reflect.Type {
+	for _, p := range path {
+		if val.Kind() == reflect.Interface {
+			return val
+		}
+		field, found := val.FieldByName(p)
+		if !found {
+			meth, found := val.MethodByName(p)
+			if !found {
+				panic("field/method not found")
+			}
+			val = meth.Type.Out(0)
+		} else {
+			val = field.Type
+		}
+	}
+	return val
+}
+
+func (t *treeTypecheck) getFuncValueType(name string) reflect.Type {
+	if f, ok := t.funcs[name]; ok {
+		fR := reflect.TypeOf(f)
+		if fR.NumOut() > 0 {
+			return fR.Out(0)
+		}
+	}
+	return nil
+}
---
+++ simplifier/typecheck_test.go
@@ -0,0 +1,382 @@
+package simplifier_test
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/mh-cbon/template-tree-simplifier/simplifier"
+)
+
+type type1 struct {
+	Some []string
+}
+
+type type2 struct {
+	Some string
+}
+
+type type3 struct {
+	Some type2
+}
+
+type type4 struct {
+	Some interface{}
+}
+
+func (t type4) Method() interface{}        { return nil }
+func (t type4) MethodArgs(a string) string { return "" }
+
+func TestTypeCheck(t *testing.T) {
+	//-
+	defFuncs := template.FuncMap{
+		"split": strings.Split,
+		"join":  func(sep string, a []string) string { return strings.Join(a, sep) },
+		"up":    strings.ToUpper,
+		"lower": strings.ToLower,
+		"incr":  func(s int) int { return s + 1 },
+		"mul":   func(s int, d int) int { return s * d },
+		"intf":  func() interface{} { return nil },
+	}
+	// create a reflect of interface{}
+	var x []interface{}
+	reflectInterface := reflect.TypeOf(x).Elem()
+	// add more test with $ and numbers and bool
+	testTable := []TestData{
+		TestData{
+			tplstr:       `{{range .Some}}{{end}}`,
+			expectTplStr: `{{$var0 := .Some}}{{range $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf(type1{}),
+					"$var0": reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{range .}}{{end}}`,
+			expectTplStr: `{{$var0 := .}}{{range $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         []string{},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf([]string{}),
+					"$var0": reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{range $x := .Some}}{{end}}`,
+			expectTplStr: `{{$var0 := .Some}}{{range $tpl_x := $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf(type1{}),
+					"$var0": reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(""),
+					"$tpl_x": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .}}{{range $x.Some}}{{end}}`,
+			expectTplStr: `{{$tpl_x := .}}{{$var0 := $tpl_x.Some}}{{range $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type1{}),
+					"$tpl_x": reflect.TypeOf(type1{}),
+					"$var0":  reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{range $x, $y := .Some}}{{end}}`,
+			expectTplStr: `{{$var0 := .Some}}{{range $tpl_x, $tpl_y := $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf(type1{}),
+					"$var0": reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(""),
+					"$tpl_x": reflect.TypeOf(1),
+					"$tpl_y": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{with $x := .Some}}{{end}}`,
+			expectTplStr: `{{$var0 := .Some}}{{with $tpl_x := $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf(type1{}),
+					"$var0": reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".":      reflect.TypeOf([]string{}),
+					"$tpl_x": reflect.TypeOf([]string{}),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .}}{{with $x.Some}}{{end}}`,
+			expectTplStr: `{{$tpl_x := .}}{{$var0 := $tpl_x.Some}}{{with $var0}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type1{}),
+					"$tpl_x": reflect.TypeOf(type1{}),
+					"$var0":  reflect.TypeOf([]string{}),
+				},
+				map[string]reflect.Type{
+					".": reflect.TypeOf([]string{}),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{with .}}{{$y := .}}{{end}}`,
+			expectTplStr: `{{$var0 := .}}{{with $var0}}{{$tpl_y := .}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf(type1{}),
+					"$var0": reflect.TypeOf(type1{}),
+				},
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type1{}),
+					"$tpl_y": reflect.TypeOf(type1{}),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{with .}}{{$y := $}}{{end}}`,
+			expectTplStr: `{{$var0 := .}}{{with $var0}}{{$tpl_y := $}}{{end}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type1{Some: []string{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":     reflect.TypeOf(type1{}),
+					"$var0": reflect.TypeOf(type1{}),
+				},
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type1{}),
+					"$tpl_y": reflect.TypeOf(type1{}),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := up .Some}}`,
+			expectTplStr: `{{$tpl_x := up .Some}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type2{Some: ""},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type2{}),
+					"$tpl_x": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := up .Some}}{{$y := $x}}`,
+			expectTplStr: `{{$tpl_x := up .Some}}{{$tpl_y := $tpl_x}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type2{Some: ""},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type2{}),
+					"$tpl_x": reflect.TypeOf(""),
+					"$tpl_y": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := ""}}`,
+			expectTplStr: `{{$tpl_x := ""}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type2{Some: ""},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type2{}),
+					"$tpl_x": reflect.TypeOf(""),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := 2}}`,
+			expectTplStr: `{{$tpl_x := 2}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type2{Some: ""},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type2{}),
+					"$tpl_x": reflect.TypeOf(2),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := true}}`,
+			expectTplStr: `{{$tpl_x := true}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type2{Some: ""},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type2{}),
+					"$tpl_x": reflect.TypeOf(true),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .Some}}{{$y := .Some.Some}}{{$z := .}}`,
+			expectTplStr: `{{$tpl_x := .Some}}{{$tpl_y := .Some.Some}}{{$tpl_z := .}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type3{Some: type2{Some: ""}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type3{}),
+					"$tpl_x": reflect.TypeOf(type2{}),
+					"$tpl_y": reflect.TypeOf(""),
+					"$tpl_z": reflect.TypeOf(type3{}),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .Some}}{{$y := .Some.Some}}{{$z := .}}`,
+			expectTplStr: `{{$tpl_x := .Some}}{{$tpl_y := .Some.Some}}{{$tpl_z := .}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type4{Some: type2{Some: ""}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type4{}),
+					"$tpl_x": reflectInterface,
+					"$tpl_y": reflectInterface,
+					"$tpl_z": reflect.TypeOf(type4{}),
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .Some}}{{$y := $x.Some}}`,
+			expectTplStr: `{{$tpl_x := .Some}}{{$tpl_y := $tpl_x.Some}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type4{Some: type2{Some: ""}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type4{}),
+					"$tpl_x": reflectInterface,
+					"$tpl_y": reflectInterface,
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .Method}}`,
+			expectTplStr: `{{$tpl_x := .Method}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type4{Some: type2{Some: ""}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type4{}),
+					"$tpl_x": reflectInterface,
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .Some.Method}}`,
+			expectTplStr: `{{$tpl_x := .Some.Method}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type4{Some: type4{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type4{}),
+					"$tpl_x": reflectInterface,
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := .Some.MethodArgs "r"}}`,
+			expectTplStr: `{{$tpl_x := .Some.MethodArgs "r"}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			data:         type4{Some: type4{}},
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(type4{}),
+					"$tpl_x": reflectInterface,
+				},
+			},
+		},
+		TestData{
+			tplstr:       `{{$x := intf}}`,
+			expectTplStr: `{{$tpl_x := intf}}`,
+			funcs:        defFuncs,
+			typecheck:    true,
+			checkedTypes: []map[string]reflect.Type{
+				map[string]reflect.Type{
+					".":      reflect.TypeOf(nil),
+					"$tpl_x": reflectInterface,
+				},
+			},
+		},
+	}
+
+	for _, testData := range testTable {
+		if execTestData(testData, t) == false {
+			break
+		}
+	}
+}
+
+func typechecktemplate(t *template.Template, testData TestData) (*template.Template, *simplifier.State) {
+	ret, err := t.Clone()
+	if err != nil {
+		panic(err)
+	}
+	var typeCheck *simplifier.State
+	for _, t := range ret.Templates() {
+		if t.Tree != nil {
+			simplifier.Simplify(t.Tree)
+			typeCheck = simplifier.TypeCheck(t.Tree, testData.data, testData.funcs)
+		}
+	}
+	return ret, typeCheck
+}
